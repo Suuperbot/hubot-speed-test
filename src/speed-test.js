eb964c57fbf4897c6eab6b736977a81e0b0a4496
latrokles <latrokles@gmail.com>
2016-07-09 20:16:56 -0700

    nicer output and tested on local hubot instance

--- src/speed-test.js
+++ src/speed-test.js
@@ -22,8 +22,19 @@ module.exports = function (robot) {
   robot.respond(/run speedtest/i, msg => {
     // instantiate our speedtest
     const test = speedTest({maxTime: 5000});
+
     test.on('data', results => {
-      msg.reply(results);
+      const speed = results.speeds;
+      let output = `here are the speed test results:\n`;
+      output += `**speeds**\n----------\n`;
+      output += `DOWNLOAD: ${speed.download} Mbps\n`;
+      output += `UPLOAD:   ${speed.upload} Mbps`;
+
+      msg.reply(output);
+    });
+
+    test.on('error', error => {
+      msg.reply('There was an error running the speed test:\n', error);
     });
   });
 };
